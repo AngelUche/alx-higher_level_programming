34454d01f23b0f4c7bd1581c8698f50c66279be1
AngelUche <ucheorji9974@gmail.com>
2022-09-21 00:33:31 -0700

    101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     """this code is used in sting slicing"""
-   if n < 0:
-       return (str)
-   str str[:n] + str([n+1):]
-   return str
+    if n < 0:
+        return (str)
+    str = str[:n] + str[(n+1):]
+    return str
