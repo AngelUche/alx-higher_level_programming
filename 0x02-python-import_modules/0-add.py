4a677123b00b6404d4a056c573886fc931d58dde
AngelUche <ucheorji9974@gmail.com>
2022-09-22 13:57:30 -0700

    debugging

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,6 +2,7 @@
 if __name__  ==  "__main__":
 
     from  add_0 import add
+
     a: int = 1
     b: int = 2
-    print(" {} + {}  = {}". format(a, b, add(a, b)))
+    print("{} + {}  = {}". format(a, b, add(a, b)))
