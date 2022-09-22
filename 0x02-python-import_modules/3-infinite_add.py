71868b8d183a63913617290957c302401de416ee
AngelUche <ucheorji9974@gmail.com>
2022-09-22 09:51:25 -0700

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            sum += int(arg)
+    print("{}".format(sum))
+
