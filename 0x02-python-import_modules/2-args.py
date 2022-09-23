d957fc00a6e1666b4cc144303527c837b5425bc7
AngelUche <ucheorji9974@gmail.com>
2022-09-22 19:57:00 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,9 @@ if __name__ == "__main__":
     i = len(sys.argv) - 1
     if i == 0:
         print("{} arguments.".format(i))
-    elif i >= 1:
+    elif i == 1:
+        print("{} arguments:".format(i))
+    else:
         print("{} arguments:".format(i))
     if i >= 1:
         i = 0
