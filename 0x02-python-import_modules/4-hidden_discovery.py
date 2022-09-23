e0442e66ac6c465f10d73c24671b70e831b610e1
AngelUche <ucheorji9974@gmail.com>
2022-09-23 04:11:06 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,5 +5,7 @@ if __name__ == "__main__":
 
     names = dir(hidden_4)
     for name in names:
-        if name[:2] != "__":
+        if name[:2] == "__":
+            pass
+        else:
             print(name)
