7aa7aa3abeb659feba41581edbcdc5d961318108
AngelUche <ucheorji9974@gmail.com>
2022-09-26 01:04:40 -0700

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    new_tuple = ()
+    len_tuple = 0
+    for letters in sentence:
+        if sentence[0] == ' ':
+            sentence[0] = None
+        len_tuple += 1
+
+    else:
+        new_tuple = sentence[0]
+    return len_tuple, new_tuple
